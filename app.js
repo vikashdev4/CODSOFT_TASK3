6d2b813f31843a548741b05d16b40f494daa0538
Vikash Dhakad <dhakadakash116@gmail.com>
2026-06-22 22:31:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+let display = document.getElementById("display");
+
+function append(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+
+    if(display.value === ""){
+        return;
+    }
+
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
